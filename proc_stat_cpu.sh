2cf4b423a0f9121a8210136cc971cd6da2f33439
Shigehiro Fukase <shigehiro.fukase@gmail.com>
2024-10-26 21:53:23 +0900

    proc_stat_cpu.sh: at start, set cursor pos bottom left of screen

--- proc_stat_cpu.sh
+++ proc_stat_cpu.sh
@@ -6,11 +6,10 @@
 #   INTERVAL_SEC:   sleep every loop
 #   HIDE_CURSOR:    0:show !0:hide cursor
 
-# [ -z "${CONFIG_INTERVAL_DEFAULT}" ] && CONFIG_INTERVAL_DEFAULT=0.1
-[ -z "${CONFIG_INTERVAL_DEFAULT}" ] && CONFIG_INTERVAL_DEFAULT=1
-[ -z "${CONFIG_HIDE_CURSOR}" ] && CONFIG_HIDE_CURSOR=1
-[ -z "${HIDE_CURSOR}" ] && HIDE_CURSOR=${CONFIG_HIDE_CURSOR}
-[ -z "${INTERVAL_SEC}" ] && INTERVAL_SEC=${CONFIG_INTERVAL_DEFAULT}
+# [ -z "${INTERVAL}" ] && INTERVAL=0.1
+[ -z "${INTERVAL}" ] && INTERVAL=1
+[ -z "${HIDE_CURSOR}" ] && HIDE_CURSOR=1
+[ -z "${INTERVAL_SEC}" ] && INTERVAL_SEC=${INTERVAL}
 
 _retval=
 retval() {
@@ -42,19 +41,31 @@ calc_per() {
     retval ${per}
 }
 
-CSICUU="\e[%uA"         # [esc] move cursor up N line
-CSICPL="\e[${Pn}F"      # [esc] move cursor up N line head
-CSIED1="\e[1J"          # [esc] clear screen top to cursor
-CSIED2="\e[2J"          # [esc] clear screen
-CSIEL0="\e[0K"          # [esc] clear cursor pos to end of line
-DECTCEMS="\e[?25h"      # [esc] DECSET DECTCEM show cursor
-DECTCEMR="\e[?25l"      # [esc] DECRST DECTCEM hide cursor
-DECALTSCRNS="\e[?47h"   # [esc] DECSET XT_ALTSCRN swap to alt screen buffer
-DECALTSCRNR="\e[?47l"   # [esc] DECRST XT_ALTSCRN swap to normal screen buffer
-DECALTS47S="\e[?1047h"  # [esc] DECSET XT_ALTS_47 swap to alt screen buffer
-DECALTS47R="\e[?1047l"  # [esc] DECRST XT_ALTS_47 clear screen, swap to normal screen buffer
-DECEXTSCRNS="\e[?1049h" # [esc] DECSET XT_EXTSCRN save cursor pos, swap to alt screen buffer, clear screen
-DECEXTSCRNR="\e[?1049l" # [esc] DECRST XT_EXTSCRN clear screen, swap to normal screen buffer, restore cursor pos
+CSIICH="\e[${Ps}@"      # Insert Ps (Blank) Character(s) (default = 1)
+CSIICH="\e[${Ps} @"     # Shift left Ps columns(s) (default = 1) (SL), ECMA-48.
+CSICUU="\e[${Ps}A"      # Cursor Up Ps Times (default = 1)
+CSICUU="\e[${Ps} A"     # Shift right Ps columns(s) (default = 1) (SR), ECMA-48.
+CSICUD="\e[${Ps}B"      # Cursor Down Ps Times (default = 1)
+CSICUF="\e[${Ps}C"      # Cursor Forward Ps Times (default = 1)
+CSICUB="\e[${Ps}D"      # Cursor Backward Ps Times (default = 1)
+CSICNL="\e[${Ps}E"      # Cursor Next Line Ps Times (default = 1)
+CSICPL="\e[${Ps}F"      # Cursor Preceding Line Ps Times (default = 1)
+CSICHA="\e[${Ps}G"      # Cursor Character Absolute  [column] (default = [row,1])
+CSICUP="\e[${Ps1};${Ps2}H" # Cursor Position [row;column] (default = [1,1])
+CSICUPBL="\e[999;1H"    # set cursor pos bottom left of screen
+CSICHT="\e[${Ps}I"      # move cursor up N line head
+CSIED="\e[${Ps}J"       # erase screen
+CSIED1="\e[1J"          # clear screen top to cursor
+CSIED2="\e[2J"          # clear screen
+CSIEL0="\e[0K"          # clear cursor pos to end of line
+DECTCEMS="\e[?25h"      # DECSET DECTCEM show cursor
+DECTCEMR="\e[?25l"      # DECRST DECTCEM hide cursor
+DECALTSCRNS="\e[?47h"   # DECSET XT_ALTSCRN swap to alt screen buffer
+DECALTSCRNR="\e[?47l"   # DECRST XT_ALTSCRN swap to normal screen buffer
+DECALTS47S="\e[?1047h"  # DECSET XT_ALTS_47 swap to alt screen buffer
+DECALTS47R="\e[?1047l"  # DECRST XT_ALTS_47 clear screen, swap to normal screen buffer
+DECEXTSCRNS="\e[?1049h" # DECSET XT_EXTSCRN save cursor pos, swap to alt screen buffer, clear screen
+DECEXTSCRNR="\e[?1049l" # DECRST XT_EXTSCRN clear screen, swap to normal screen buffer, restore cursor pos
 NL="${CSIEL0}\n"        # new line
 
 cpu_stat() {
@@ -64,7 +75,7 @@ cpu_stat() {
          awk '{if($1 ~ /^cpu/) print "linenum="NR" "$1"=( "$2" "$3" "$4" "$5" "$6" "$7" "$8" )"}' /proc/stat
     )
     let local num_cpu=(${linenum}-1)
-    Pn=$((${linenum}+2)) # CSICPL param
+    Ps=$((${linenum}+2)) # CSICPL param
     local LABEL_ALL="ALL(${num_cpu})"
     local SCRBUF=""
     SCRBUF="${SCRBUF}${DECTCEMR}" # hide cursor
@@ -136,6 +147,7 @@ trap 'exit_handler' EXIT
 trap 'exit_handler' INT TERM
 
 altscrn_enter   # Enter to ALT screen
+printf "${CSICUPBL}" # set cursor pos bottom left of screen
 
 [ ${HIDE_CURSOR} -ne 0 ] && printf "${DECTCEMR}" # hide cursor
 for ((count=0; ; count++));  do
